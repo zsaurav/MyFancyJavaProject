8e604af4a7226760678f4d5c1262f1741d56f16e
msajib <msajib191028@bscse.uiu.ac.bd>
2022-05-10 23:36:32 +0600

    Y has changed the file

--- Main.java
+++ Main.java
@@ -1,15 +1,60 @@
-import java.util.Scanner;
+interface Calculator
+{
+	void result();
+}
 
-public class Main {
-    public static void main(String[] args) {
-        //This is a simple calculator
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the first number");
-        int a = sc.nextInt();
-        System.out.println("Enter the second number");
-        int b = sc.nextInt();
 
-        int sum = a+b;
-        System.out.println("The summation of the numbers is: " + sum);
-    }
+class Adder implements Calculator
+{
+	 @Override
+	 public void result()
+	 {
+		 System.out.println("The sum of the two numbers is: ");
+	 }
+}
+
+
+class Subtractor implements Calculator
+{
+	 @Override
+	 public void result()
+	 {
+		 System.out.println("The subtraction of the two numbers is: ");
+	 }
+}
+
+
+class CalculatorFactory
+{
+ 
+	 public Calculator getCommand(String type) throws Exception
+	 {
+		 switch (type)
+		 {
+			 case "Add":
+				 return new Adder();
+			 case "Subtract":
+				 return new Subtractor();
+			 default:
+				 throw new Exception( "Command type : "+type+" cannot be instantiated");
+		 }
+	 }
+}
+
+class Main
+{
+	 public static void main(String[] args) throws Exception
+	 {
+		 
+		 CalculatorFactory calculatorFactory = new CalculatorFactory();
+		 Calculator adder=calculatorFactory.getCommand("Add");
+		 adder.result();
+
+		 Calculator subtractor=calculatorFactory.getCommand("Subtract");
+		 subtractor.result();
+
+		 Calculator multiplier=calculatorFactory.getCommand("Multiply");
+		 multiplier.result();
+
+	 }
 }
